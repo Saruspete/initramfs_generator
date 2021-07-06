d898cbb785f0ea9537f19879cc37c8de0819f3d2
Adrien Mahieux <adrien.mahieux@gmail.com>
2021-07-06 23:59:24 +0200

    [Enh] Add kernel modules management

--- generate.sh
+++ generate.sh
@@ -18,13 +18,14 @@ fi
 # Load the required libraries
 ammLib::Require "optparse" "pkg" "string"
 
+ammExec::Require "modinfo" "awk" "head" "tr" "head" "cp" "readlink"
+
 typeset    PATH_ROOT=""
 typeset    PATH_TEMP="$MYPATH/img"
-typeset    PATH_IMAGE="$MYPATH/initramfs-$(date "+%Y-%m-%d_%H-%M-%S")"
+typeset    PATH_IMAGE="$MYPATH/initramfs-$(date "+%Y-%m-%d_%H-%M-%S").img"
 typeset    PATH_LDLIB=""
 
 typeset    CORE_INIT="$MYPATH/init"
-typeset -a ADD_EXTRA=()
 
 ammOptparse::AddOptGroup "path" "Path for image generation"
 ammOptparse::AddOpt "-t|--tmp="    "Temporary path to store files under"   "$PATH_TEMP"
@@ -32,23 +33,38 @@ ammOptparse::AddOpt "-o|--image="  "Generated image path"                  "$PAT
 ammOptparse::AddOpt "-r|--chroot=" "chroot source to take bin from"        "$PATH_ROOT"
 ammOptparse::AddOpt    "--ldlib="  "Additionnal LD_LIBRARY_PATH to set"    "$PATH_LDLIB"
 
+# Early parsing for chroot
+ammOptparse::Parse "chroot"
+PATH_ROOT="$(ammOptparse::Get "chroot")"
+typeset -a KERN_VERSIONS=()
+typeset    KERN_MODPATH="$PATH_ROOT/lib/modules"
+if [[ -d "$KERN_MODPATH" ]]; then
+	for i in "$KERN_MODPATH"*; do
+		typeset k="${i##*/modules/}"
+		k="${k%%/*}"
+		KERN_VERSIONS+=($k)
+	done
+fi
+[[ -z "${KERN_VERSION:-}" ]] && KERN_VERSIONS=("$(uname -r)")
+
 ammOptparse::AddOptGroup "core" "Core elements"
-ammOptparse::AddOpt "-i|--init="  "init to be used"                       "$CORE_INIT"
+ammOptparse::AddOpt "-i|--init="    "init to be used"                       "$CORE_INIT"
+ammOptparse::AddOpt "-s|--scripts=" "scripts to be run"
 
 ammOptparse::AddOptGroup "extra"     "Extra content to add"
-ammOptparse::AddOpt "-a|--add-file@"  "Add an extra file or folder into initramfs"
-
+ammOptparse::AddOpt "-a|--add-path@"  "Add an extra file or folder into initramfs"
+ammOptparse::AddOpt "-m|--add-kmod@"  "Add an extra kernel module into initramfs"
+ammOptparse::AddOpt "-k|--kernel@"    "Set one or more kernel version to search modules for" "${KERN_VERSIONS[@]}"
 
 ammOptparse::Parse --no-unknown || ammLog::Die "Options parsing error"
 
-PATH_ROOT="$(ammOptparse::Get "chroot")"
 PATH_TEMP="$(ammOptparse::Get "tmp")"
 PATH_IMAGE="$(ammOptparse::Get "image")"
 PATH_LDLIB="$(ammOptparse::Get "ldlib")"
 
 CORE_INIT="$(ammOptparse::Get "init")"
-typeset -a ADD_EXTRA=$(ammOptparse::Get "add-file")
-
+typeset -a ADD_PATHS=$(ammOptparse::Get "add-path")
+typeset -a ADD_KMODS=$(ammOptparse::Get "add-kmod")
 typeset CHROOT_LDLIB_PATH=""
 
 # Use absolute path
@@ -62,56 +78,111 @@ if [[ -n "$PATH_ROOT" ]]; then
 fi
 
 
+# @description  Find a kernel module from its name or alias
+function kmodFindPath {
+	typeset kmodName="$1"
+	typeset kernVers="${2:-$(uname -r)}"
+	typeset basePath="${3:-}"
+
+	modinfo --basedir "$basePath" -k "$kernVers" -F filename "$kmodName"
+}
+
+typeset -A __KMOD_DEPENDS_FOUND
+# @description  Find the dependency tree of a kernel module
+# @arg $1  (string) kernel module name or path
+# @arg $2  (string) (optional) Kernel version to be used. default: current kernel
+# @arg $3  (string) (optional) chroot to search into. default: live system
+# @arg $4  (int) (internal: do no provide) depth level of recursion
+# @stdout  (string[]) list of module depedencies
+function kmodDepends {
+	typeset kmodName="$1"
+	typeset kernVers="${2:-$(uname -r)}"
+	typeset basePath="${3:-}"
+	typeset -i level="${4:-0}"
+
+	# Reset on first call
+	if [[ "$level" == "0" ]]; then
+		__KMOD_DEPENDS_FOUND=()
+		# Validate entry
+		modinfo --basedir "$basePath" -k "$kernVers" -F name $kmodName
+		[[ $? -ne 0 ]] && return
+	fi
+	level+=1
+
+	# If a file is provided, guess if we can use its path to extract basePath and kernel version
+	if [[ "${kmodName:0:1}" == "/" ]] && [[ -s "$kmodName" ]]; then
+		if [[ "${kmodName#*/lib/modules/[0-9]*}" != "$kmodName" ]]; then
+			basePath="${kmodName%/lib/modules/*}"
+			kernVers="${kmodName##*/lib/modules/}"
+			kernVers="${kernVers%%/*}"
+		fi
+	fi
+
+	typeset depsLine
+	while read depsLine; do
+		[[ -z "$depsLine" ]] && break
+		typeset deps="${depsLine//,/ }"
+		typeset dep=
+		# Replace - with _ to be constant in naming
+		for dep in ${deps//-/_}; do
+			# Only display once
+			if ( set +u; [[ -z "${__KMOD_DEPENDS_FOUND[$dep]}" ]] ); then
+				echo -n "$dep "
+				# Recurse
+				$FUNCNAME "$dep" "$kernVers" "$basePath" "$level"
+			fi
+
+			__KMOD_DEPENDS_FOUND[$dep]+="$kmodName "
+		done
+
+	done < <(modinfo --basedir "$basePath" -k "$kernVers" -F depends "$kmodName" 2>/dev/null)
+	
+}
+
+# @description  Reimplementation ldd
 function _ldd {
-	typeset bin="$1" 
+	typeset bin="$1"
 
 	# If we have PATH_ROOT set, we need to call directly within the env (and skip ldd)
 	if [[ -n "$PATH_ROOT" ]]; then
 		export LD_LIBRARY_PATH+="$CHROOT_LDLIB_PATH"
-		# test multiple runtime ld
-		typeset rtldout= rtld= rtlds="$PATH_ROOT/lib64/ld-linux-x86-64.so.2 $PATH_ROOT/lib/ld-linux.so.2 $PATH_ROOT/libx32/ld-linux-x32.so.2 /lib/ld-linux.so.2 /lib64/ld-linux-x86-64.so.2"
-		for rtld in $rtlds; do
-
-			# Check if current rtld can handle given bin
-			[[ -x "$rtld" ]] || continue
-			rtldout="$($rtld --verify "$bin")"
-			case $? in
-				[02]) ;;
-				*) continue
-			esac
-
-			# Use ld to run bin and deref
-			rtldout="$(LD_TRACE_LOADED_OBJECTS=1 "$rtld" "$bin")"
-
-			typeset lib arrow file address _junk
-			echo "$rtldout" | while read lib arrow file address _junk; do
-				[[ "$lib" == "linux-vdso.so.1" ]] && continue
-
-				# When there is no arrow, either lib is abs, either it's name matches
-				if [[ "$arrow" != "=>" ]]; then
-					continue
-					# Absolute or relative to PWD
-					#if [[ "${lib:0:1}" == "/" ]]; then
-					#	echo "$lib"
-					#elif [[ "${arrow:0:1}" == "(" ]]; then
-					#	echo "$(pwd -P)/$lib"
-					#fi
-				fi
-
-				# Empty file: maybe vdso or ld-linux or error msg "not a dynamic executable"
-				[[ -n "$file" ]] || continue
-
-				if [[ -e "$file" ]]; then
-					echo "$file"
-				else
-					echo >&2 "$lib"
-				fi
-			done
-		done
-
-	else
-		ldd "$1" | awk 'NF>3 && $1 != "libc.so.6"{print $3}'
+		echo >&2 "$LD_LIBRARY_PATH"
 	fi
+
+	# test multiple runtime ld
+	typeset rtldout= rtld= rtlds="$PATH_ROOT/lib64/ld-linux-x86-64.so.2 $PATH_ROOT/lib/ld-linux.so.2 $PATH_ROOT/libx32/ld-linux-x32.so.2 /lib/ld-linux.so.2 /lib64/ld-linux-x86-64.so.2"
+	for rtld in $rtlds; do
+
+		# Check if current rtld can handle given bin
+		[[ -x "$rtld" ]] || continue
+		rtldout="$($rtld --verify "$bin")"
+		case $? in
+			[02]) ;;
+			*) continue
+		esac
+
+		# Use ld to run bin and deref
+		rtldout="$(LD_TRACE_LOADED_OBJECTS=1 "$rtld" "$bin")"
+
+		typeset lib arrow file address _junk
+		echo "$rtldout" | while read lib arrow file address _junk; do
+			[[ "$lib" == "linux-vdso.so.1" ]] && continue
+
+			# When there is no arrow, either lib is abs, either it's name matches
+			if [[ "$arrow" != "=>" ]]; then
+				continue
+			fi
+
+			# Empty file: maybe vdso or ld-linux or error msg "not a dynamic executable"
+			[[ -n "$file" ]] || continue
+
+			if [[ -e "$file" ]]; then
+				echo "$file"
+			else
+				echo >&2 "$lib"
+			fi
+		done
+	done
 }
 
 typeset -A __DEPS_FOUND=
@@ -120,16 +191,24 @@ function binFindDeps {
 	typeset file
 	for file in "$@"; do
 
-		if ! [[ -e "$file" ]]; then
-			file="$(binFind "$file")"
+		# Try to find from the shortname (honoring PATH_ROOT)
+		typeset filePath="$file"
+		if ! [[ -e "$filePath" ]]; then
+			filePath="$(binFind "$file")"
+		fi
+
+		# cannot find any matching file... skipping
+		if ! [[ -e "$filePath" ]]; then
+			ammLog::Warning "Skipping unresolvable '$file'"
+			continue
 		fi
 
 		# Try to get the real binary
-		typeset realbin="$(realpath "$file")"
+		typeset realbin="$(realpath "$filePath")"
 		echo "$realbin"
 
 		# Check if shebang
-		typeset firstline="$(head -n1 "$file" 2>/dev/null|tr -cd '[:print:]')" interp=""
+		typeset firstline="$(head -n1 "$realbin" 2>/dev/null|tr -cd '[:print:]')" interp=""
 		if [[ "${firstline:0:2}" == "#!" ]]; then
 			interp="${firstline:2}"
 			interp="$(ammString::Trim "${interp%% *}")"
@@ -247,7 +326,22 @@ chrootFileAdd /dev/{std{err,in,out},fd}
 # Copy kernel modules
 #
 # #############################################################################
-
+if [[ -n "${ADD_KMODS:-}" ]]; then
+	for kmodNeed in "${ADD_KMODS[@]}"; do
+		for krn in "${KERN_VERSIONS[@]}"; do
+			ammLog::StepBegin "Adding module '$kmodNeed' and dependencies, version '$krn' in '${PATH_ROOT:-/}'"
+			for kmod in $(kmodDepends "$kmodNeed" "$krn" "$PATH_ROOT"); do
+				typeset filePath="$(kmodFindPath "$kmod" "$krn" "$PATH_ROOT")"
+				ammLog::Debug "Found for '$kmod': '$filePath'"
+				if [[ -n "$filePath" ]]; then
+					ammLog::Info "Adding mod '$kmod' ($filePath)"
+					chrootFileAdd "$filePath@${filePath#${PATH_ROOT}}"
+				fi
+			done
+			ammLog::StepEnd
+		done
+	done
+fi
 
 # #############################################################################
 #
@@ -267,11 +361,11 @@ chrootFileAdd $(binFindDeps mount)
 chrootFileAdd $(binFindDeps ip devlink ethtool dhcpcd wget curl)
 
 # Stress-test
-chrootFileAdd $MYPATH/extra/mersenne/lib* $(binFindDeps $MYPATH/extra/mersenne/mprime)
+chrootFileAdd $MYPATH/extra/mersenne $(binFindDeps $MYPATH/extra/mersenne/mprime)
 
 # Extra packages
-if [[ -n "${ADD_EXTRA:-}" ]]; then
-	chrootFileAdd "${ADD_EXTRA[@]}"
+if [[ -n "${ADD_PATHS:-}" ]]; then
+	chrootFileAdd "${ADD_PATHS[@]}"
 fi
 
 
@@ -280,21 +374,31 @@ fi
 # Generate cpio archive
 #
 # #############################################################################
+ammLog::StepBegin "Creating archive from '$PATH_TEMP'"
 (
 	cd "$PATH_TEMP"
-	find . -print0 | cpio --null -ov --format=newc | gzip > "$PATH_IMAGE"
+	find . -print0 | cpio --null -o --format=newc | gzip > "$PATH_IMAGE"
 )
+ammLog::Info "Generated: '$PATH_IMAGE'"
+ammLog::StepEnd
 
-echo "Generated: '$PATH_IMAGE'"
+# Create a symlink for testing
 typeset imgLatest="${PATH_IMAGE%/*}/initramfs-latest.img"
+ammLog::Info "Creating symlink '$imgLatest'"
 [[ -L "$imgLatest" ]] && rm "$imgLatest"
 ln -s "$PATH_IMAGE" "$imgLatest"
 
-echo "You can test it with:"
+# Quick test with qemu
+typeset kernCurrent="$(</proc/cmdline)"
+kernCurrent="${kernCurrent#BOOT_IMAGE=}"
+kernCurrent="${kernCurrent%% *}"
+kernCurrent="${kernCurrent#(*)}"
+
+ammLog::Info "You can test it with:"
 cat <<-EOT
-	qemu-system-x86_64 -cpu Skylake-Client \
-	-no-reboot -nographic \
-	-append "console=ttyS0 panic=5" \
-	-kernel /boot/kernel-genkernel-x86_64-5.4.109-gentoo \
-	-initrd $PATH_IMAGE
-	EOT
+qemu-system-x86_64 -cpu Skylake-Client \
+-no-reboot -nographic \
+-append "console=ttyS0 panic=-1" \
+-kernel /boot/$kernCurrent \
+-initrd $imgLatest
+EOT
